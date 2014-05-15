2fe01d3d99ce87edea5a761820938f2f2064f5fb
zeke <zeke@sikelianos.com>
2014-05-14 20:47:24 -0700

    all the things

---
+++ README.md
@@ -0,0 +1,6 @@
+## app-registry
+
+An HTTP interface to the app.json registry, backed by a [GitHub wiki](https://github.com/heroku/app.json/wiki) datastore.
+
+- https://github.com/heroku/app.json/wiki
+- https://github.com/heroku/app.json/wiki/Registry
--- test/registry.js
+++ test/registry.js
@@ -3,10 +3,9 @@ require("dotenv").load()
 var assert = require("assert")
 var fs = require("fs")
 var util = require("util")
-var Registry = require("..")
+var Registry = require("../lib/registry")
 
 describe("Registry", function() {
-  this.timeout(3000)
   var registry
 
   before(function(done) {
@@ -35,5 +34,23 @@ describe("Registry", function() {
     })
   })
 
+  it("publish", function(done) {
+    this.timeout(3000)
+    registry.entries = [
+      "https://github.com/zeke/sample0",
+      "https://github.com/zeke/sample1",
+      "https://github.com/zeke/sample3"
+    ]
+    var app = "https://github.com/zeke/sample2"
+    registry.publish(app, function(err) {
+      console.error(err)
+      console.error(err.error)
+      assert(!err)
+      assert.equal(registry.entries.length, 4)
+      console.log("almost done")
+      // assert.equal(registry.entries.indexOf(app), 2)
+      done()
+    })
+  })
 
 })
